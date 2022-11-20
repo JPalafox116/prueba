87a852c18700fc970805cd3c6a585b05229e9f23
My Pham <mpham@macbook-pro.myfiosgateway.com>
2022-11-20 00:10:24 -0500

    Add github pages and hash router to make page work on github url

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import "./App.css";
 import "./components/styles.css";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter, HashRouter } from "react-router-dom";
 import Main from "./components/Main";
 import PokemonDetail from "./components/PokemonDetail/PokemonDetail";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <>
         <Routes>
           <Route exact path="/" element={<Main />} />
           <Route exact path="/:category" element={<PokemonDetail />} />
         </Routes>
       </>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
